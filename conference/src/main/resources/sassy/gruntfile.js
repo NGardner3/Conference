54a464008e0c999c5749a179a8e6c37d918b8b46
Nikki <nicole4qb@gmail.com>
2020-07-25 15:43:45 -0400

    Gruntfile configured.

---
+++ conference/src/main/resources/sassy/gruntfile.js
@@ -0,0 +1,17 @@
+    //The "wrapper" function
+  module.exports = function(grunt) {
+      //Project and task configuration
+      grunt.initConfig({
+	     pkg: grunt.file.readJSON('package.json'),
+	
+	
+});
+  
+
+   // Loading Grunt plugins and tasks
+   require('load-grunt-tasks')(grunt);
+
+   //Custom tasks
+   grunt.registerTask('default', []);
+
+};
